5111f39330e24fdaf2cb18a2d4c5b170b21c3792
Thomas Kennedy <tck9@students.uwf.edu>
2020-08-30 20:27:06 -0500

    Did the thing

---
+++ Project1/Lab1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "tkc9-s:ThomasKennedy" + endl;
+
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 5; j++) {
+			cout << i * j;
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
